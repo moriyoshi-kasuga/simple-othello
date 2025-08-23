d315f2d88a74809a4bd89f24ca53cde748655a5d
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-08-23 20:49:50 +0900

    fix: missmatch type of log::Level

--- client/src/main.rs
+++ client/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let level = std::env::var("RUST_LOG")
         .ok()
         .and_then(|s| s.parse().ok())
-        .unwrap_or(log::LevelFilter::Info);
+        .unwrap_or(log::Level::Info);
     console_log::init_with_level(level).expect("Failed to initialize logger");
 
     yew::Renderer::<client::App>::new().render();
