6f7c5d42920a27690ef6993a42030a1b7028adf0
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-08-23 21:23:57 +0900

    fix: remove auto impl ReqPacket and ResPacket

--- net/src/models/mod.rs
+++ net/src/models/mod.rs
@@ -2,8 +2,4 @@ use serde::{Serialize, de::DeserializeOwned};
 
 pub trait ReqPacket: DeserializeOwned + Serialize + Send + Sync + 'static {}
 
-impl<T> ReqPacket for T where T: DeserializeOwned + Serialize + Send + Sync + 'static {}
-
 pub trait ResPacket: DeserializeOwned + Serialize + Send + Sync + 'static {}
-
-impl<T> ResPacket for T where T: DeserializeOwned + Serialize + Send + Sync + 'static {}
